38f2e3fe82b797117ef72746977d922295307838
yamakazeAnkk <tuongankk2003@gmail.com>
2025-01-03 22:45:26 +0700

    change string convert object

--- Models/SensorData.cs
+++ Models/SensorData.cs
@@ -11,7 +11,7 @@ namespace GreenIotApi.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        public ObjectId Id { get; set; }
         public float Temperature { get; set; }
         public float Humidity { get; set; }
         public float SoilMoisture { get; set; }
